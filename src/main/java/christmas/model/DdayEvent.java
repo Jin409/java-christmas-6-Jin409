342739c3fc8e36343fe6670735db9ef39fbeecae
Jin409 <sualng123@gmail.com>
2024-11-18 15:45:59 +0900

    feat: 디데이 할인 이벤트를 추가해라

---
+++ src/main/java/christmas/model/DdayEvent.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public enum DdayEvent {
+    D_DAY(1, 25, (visitDate) -> (long) ((visitDate - 1) * 100L + 1_000));
+
+    private final int startDate;
+    private final int endDate;
+    private final Function<Integer, Long> discountedAmount;
+
+    DdayEvent(int startDate, int endDate, Function<Integer, Long> discountedAmount) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.discountedAmount = discountedAmount;
+    }
+
+    public static long getDiscountedAmount(int visitDate) {
+        long discountedAmount = 0;
+
+        for (DdayEvent dDayEvent : DdayEvent.values()) {
+            if (visitDate > dDayEvent.startDate && visitDate < dDayEvent.endDate) {
+                discountedAmount += dDayEvent.discountedAmount.apply(visitDate);
+            }
+        }
+        return discountedAmount;
+    }
+}
--- src/main/java/christmas/model/Event.java
+++
@@ -1,7 +0,0 @@
-package christmas.model;
-
-import java.time.LocalDate;
-
-public interface Event {
-    long getDiscountedAmount(LocalDate visitDate);
-}
