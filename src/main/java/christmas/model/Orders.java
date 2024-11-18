27a793b1c29c8a5e74a1bd9733ee73fa839360a2
Jin409 <sualng123@gmail.com>
2024-11-18 16:03:48 +0900

    feat: 고객이 주문의 규칙을 준수하도록 해라

--- src/main/java/christmas/model/Orders.java
+++ src/main/java/christmas/model/Orders.java
@@ -3,12 +3,37 @@ package christmas.model;
 import java.util.List;
 
 public class Orders {
+    private static final int VALID_SIZE = 20;
+
     private final List<Order> orders;
     private final int visitDate;
 
     public Orders(int visitDate, List<Order> orders) {
+        validate(orders);
+
         this.visitDate = visitDate;
         this.orders = orders;
+    }
+
+    private void validate(List<Order> orders) {
+        if (orders.size() > VALID_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        if (containsOnlyDrink(orders)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean containsOnlyDrink(List<Order> orders) {
+        boolean containsOnlyDrink = true;
+        for (Order order : orders) {
+            if (order.doesOrderBelongTo(MenuCategory.DRINK)) {
+                continue;
+            }
+            containsOnlyDrink = false;
+        }
+        return containsOnlyDrink;
     }
 
     public long getQuantityOf(MenuCategory applicableCategory) {
